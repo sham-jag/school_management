4ddf00b2d54912e704336738b9069dc4de3638c9
sham-jag <sjathavane@gmail.com>
2021-08-15 19:53:11 +0530

    Second commit

---
+++ src/main/java/com/example/school/scheduler/ReportGenerator.java
@@ -0,0 +1,30 @@
+package com.example.school.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.example.school.service.StudentService;
+import com.example.school.service.TeacherService;
+
+@Component
+public class ReportGenerator {
+
+	private Logger logger = LoggerFactory.getLogger(ReportGenerator.class);
+	
+	@Autowired
+	private StudentService s;
+	
+	@Autowired
+	private TeacherService t;
+	
+	
+	@Scheduled(cron = "*/10 * * * * *")
+	public void generateReport() {
+		logger.info("***************  REPORT ***************");
+		logger.info("Total teachers : " + t.getTeachers().size());
+//		logger.info("Total students : " + s.getStudents().size());
+	}
+}
